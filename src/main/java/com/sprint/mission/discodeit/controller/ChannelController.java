55cff6a830aaa46c6a8c16b365c824b0fa26bfdf
kwakinsung <kis2690@gmail.com>
2026-02-09 15:20:08 +0900

    채널 관리 API 요구사항 만족

---
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -0,0 +1,58 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.data.ChannelDto;
+import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelUpdateRequest;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.service.ChannelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/channels")
+@RequiredArgsConstructor
+public class ChannelController {
+
+    private final ChannelService channelService;
+
+    @RequestMapping(value = "/public", method = RequestMethod.POST)
+    @ResponseBody
+    public Channel createPublicChannel(@RequestBody PublicChannelCreateRequest request) {
+        return channelService.create(request);
+
+    }
+
+    @RequestMapping(value = "/private", method = RequestMethod.POST)
+    @ResponseBody
+    public Channel createPrivateChannel(@RequestBody PrivateChannelCreateRequest request) {
+        return channelService.create(request);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+    @ResponseBody
+    public Channel updatePublicChannel(@PathVariable UUID id,
+                                       @RequestBody PublicChannelUpdateRequest request){
+
+        return channelService.update(id, request);
+
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteChannel(@PathVariable UUID id){
+        channelService.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<ChannelDto> getPublicChannels(@RequestParam UUID userId){
+        return channelService.findAllByUserId(userId);
+    }
+
+}
